588099c983418a4e7784502c0a410f32365ce228
SuperPomme2Ter <SuperPomme2Ter@gmail.com>
2026-05-21 11:59:29 +0200

    fix ?

--- PC/LETMEDRAWINPC/main.c
+++ PC/LETMEDRAWINPC/main.c
@@ -7,11 +7,12 @@
 #include "Client.h"
 #include "Server.h"
 #include <windows.h>
-#include <commctrl.h>
+#include <gtk/gtk.h>
 
 // POUR LE SAINT AMOUR DES VECTREX REFACTO ET NETTOIE MOI CE CODE FOU QUE JE SUIS AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAH
 // Je crois bien qu'avec L'interface Graphique ça va être pire encore...
 
+/*
 #ifndef UNICODE
 #define UNICODE
 #endif 
@@ -111,7 +112,49 @@ int CreateInputMouse(INPUT*(*inputs)[2], int size, ...) {
 
 
 }
+*/
+static void
+print_hello (GtkWidget *widget,
+             gpointer   data)
+{
+    g_print ("Hello World\n");
+}
 
+static void
+activate (GtkApplication *app,
+          gpointer        user_data)
+{
+    GtkWidget *window;
+    GtkWidget *button;
+
+    window = gtk_application_window_new (app);
+    gtk_window_set_title (GTK_WINDOW (window), "Hello");
+    gtk_window_set_default_size (GTK_WINDOW (window), 200, 200);
+
+    button = gtk_button_new_with_label ("Hello World");
+    gtk_widget_set_halign(button, GTK_ALIGN_CENTER);
+    gtk_widget_set_valign(button, GTK_ALIGN_CENTER);
+    g_signal_connect (button, "clicked", G_CALLBACK (print_hello), NULL);
+    gtk_window_set_child (GTK_WINDOW (window), button);
+
+    gtk_window_present (GTK_WINDOW (window));
+}
+
+int
+main (int    argc,
+      char **argv)
+{
+    GtkApplication *app;
+    int status;
+
+    app = gtk_application_new ("org.gtk.example", G_APPLICATION_DEFAULT_FLAGS);
+    g_signal_connect (app, "activate", G_CALLBACK (activate), NULL);
+    status = g_application_run (G_APPLICATION (app), argc, argv);
+    g_object_unref (app);
+
+    return status;
+}
+/*
 LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
 
 
@@ -397,3 +440,4 @@ LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
     
     //return 0;
 //}
+*/
