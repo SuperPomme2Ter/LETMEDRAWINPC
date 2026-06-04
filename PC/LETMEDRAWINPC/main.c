834dacffe4d08c6e6a5c30e55e6eb692f3a36f9b
SuperPomme2Ter <SuperPomme2Ter@gmail.com>
2026-06-04 15:14:09 +0200

    THIS FUCKING APP WORK
    GOT A WINDOW OPENED !
    I swear by all the Holy Vectrex, i'm gonna sleep better knowing this is working

--- PC/LETMEDRAWINPC/main.c
+++ PC/LETMEDRAWINPC/main.c
@@ -12,11 +12,11 @@
 // POUR LE SAINT AMOUR DES VECTREX REFACTO ET NETTOIE MOI CE CODE FOU QUE JE SUIS AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAH
 // Je crois bien qu'avec L'interface Graphique ça va être pire encore...
 
-/*
+
 #ifndef UNICODE
 #define UNICODE
 #endif 
-
+/*
 void CreateInputs(INPUT*(* a)[12][2],const int* inputSize) {
 
     for(int i=0;i<12;i++) {
@@ -153,106 +153,6 @@ main (int    argc,
     g_object_unref (app);
 
     return status;
-}
-/*
-LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
-
-
-int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
-    PSTR lpCmdLine, int nCmdShow)
-
-{
-    
-    INITCOMMONCONTROLSEX icex;
-    
-    // Ensure that the common control DLL is loaded. 
-    icex.dwSize = sizeof(INITCOMMONCONTROLSEX);
-    icex.dwICC  = ICC_INTERNET_CLASSES ;
-    InitCommonControlsEx(&icex); 
-
-    // Register the window class.
-    const LPCSTR CLASS_NAME  = "Sample Window Class";
-
-    
-    WNDCLASS wc = { };
-
-    wc.lpfnWndProc   = WindowProc;
-    wc.hInstance     = hInstance;
-    wc.lpszClassName = CLASS_NAME;
-
-    RegisterClass(&wc);
-
-    // Create the window.
-
-    HWND hwnd = CreateWindowEx(
-        0,                              // Optional window styles.
-        CLASS_NAME,                     // Window class
-        "Learn to Program Windows",    // Window text
-        WS_OVERLAPPEDWINDOW,            // Window style
-
-        // Size and position
-        CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT,
-
-        NULL,       // Parent window    
-        NULL,       // Menu
-        hInstance,  // Instance handle
-        NULL        // Additional application data
-        );
-
-    if (hwnd == NULL)
-    {
-        return 0;
-    }
-
-    ShowWindow(hwnd, nCmdShow);
-    
-    
-    // Create the IPAddress control.        
-    HWND hWndIPAddressFld = CreateWindow(WC_IPADDRESS, 
-                                "", 
-                                WS_CHILD | WS_OVERLAPPED | WS_VISIBLE, 
-                                0, 
-                                0,
-                                150, 
-                                20,  
-                                hwnd, 
-                                NULL, 
-                                hInstance, 
-                                NULL); 
-
-    // Run the message loop.
-
-    MSG msg = { };
-    while (GetMessage(&msg, NULL, 0, 0) > 0)
-    {
-        TranslateMessage(&msg);
-        DispatchMessage(&msg);
-    }
-    return 0;
-}
-LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
-{
-    switch (uMsg)
-    {
-    case WM_DESTROY:
-        PostQuitMessage(0);
-        return 0;
-
-    case WM_PAINT:
-        {
-            PAINTSTRUCT ps;
-            HDC hdc = BeginPaint(hwnd, &ps);
-
-            // All painting occurs here, between BeginPaint and EndPaint.
-
-            FillRect(hdc, &ps.rcPaint, (HBRUSH) (COLOR_WINDOW+1));
-
-            EndPaint(hwnd, &ps);
-        }
-        return 0;
-
-    }
-    return DefWindowProc(hwnd, uMsg, wParam, lParam);
 }
 
 //int main(void) {
@@ -440,4 +340,4 @@ LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
     
     //return 0;
 //}
-*/
+
